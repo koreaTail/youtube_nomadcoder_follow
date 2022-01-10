b06dd21360be04719e11a8d064a3829d2f2c60de
leesangjun <prokjun@gmail.com>
2022-01-11 07:44:38 +0900

    #4.1 Making Our Routers (11:05)

--- src/server.js
+++ src/server.js
@@ -1,6 +1,29 @@
-import express from "express";
+import express, { Router } from "express";
 
 const app = express();
+
+const globalRouter = express.Router();
+
+const handelHome = (req, res) => res.send("HOME");
+
+globalRouter.get("/", handelHome)
+
+const userRouter = express.Router();
+
+const handelEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit/", handelEditUser)
+
+const videoRouter = express.Router();
+
+const handelWatchVideo = (req, res) => res.send("Watch Video");
+
+VideoRouter.get("/watch/", handelWatchVideo)
+
+
+app.use("/", globalRouter);
+app.use("/users", userRouter)
+app.use("/videos", videoRouter);
 
 const handleListening = () => console.log("Server listening on port 8000 🛰")
 
