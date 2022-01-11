af692cad61da7b631f6c71875ed0fa5b5762c8e8
leesangjun <prokjun@gmail.com>
2022-01-11 09:42:08 +0900

    #3.11 External Middlewares (06:31)

--- src/server.js
+++ src/server.js
@@ -1,8 +1,33 @@
 import express from "express";
 
+const PORT = 4000;
 const app = express();
 
-const handleListening = () => console.log("Server listening on port 8000 🛰")
 
-app.listen(8000, handleListening)
+const routerLogger = (req, res, next) => {
+    console.log("PATH", req.path);
+    next();
+};
+
+const methodLogger = (req, res, next) => {
+    console.log("METHOD", req.method);
+    next();
+};
+
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+};
+
+const handleHome = (req, res) => {
+    return res.send("I still love you.");
+};
+
+
+app.use(methodLogger, routerLogger);
+app.get("/",logger, handleHome);
+
+const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
+
+app.listen(4000, handleListening);
 
