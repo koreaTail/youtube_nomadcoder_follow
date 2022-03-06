9143d4d4c1690ee17d7004f4879aff3d22433bc1
leesangjun <prokjun@gmail.com>
2022-03-07 08:57:11 +0900

    #4.6 후반

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,2 +1,3 @@
 export const trending = (req, res) => res.send("Home Page Videos");
 export const watch = (req, res) => res.send("watch video")
+export const search = (req, res) => res.send("search")
