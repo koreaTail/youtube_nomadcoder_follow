ecc465c5058c4c8870923d322a2c3c2536b63341
leesangjun <prokjun@gmail.com>
2022-03-10 15:47:38 +0900

    #4 finish

--- src/server.js
+++ src/server.js
@@ -7,7 +7,8 @@ const PORT = 4000;
 const app = express();
 
 
-
+app. set("view engine", "pug");
+app.set("views", process.cwd() + "/src" + "/views");
 app.use("/", globalRouter);
 app.use("/users", userRouter)
 app.use("/videos", videoRouter);
