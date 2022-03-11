bd7b11c2972288af81c8652fef837d362c76794a
leesangjun <prokjun@gmail.com>
2022-03-11 14:23:27 +0900

    #5.1 복습

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,54 @@
-export const trending = (req, res) => res.render("home");
+let videos = [
+    {
+        title: "첫번째 영상",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 56,
+        id: 1,
+    },
+    {
+        title: "두번째 영상",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 56,
+        id: 2,
+    },
+    {
+        title: "세번째 영상",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 56,
+        id: 3,
+    },
+    {
+        title: "넷번째 영상",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 56,
+        id: 4,
+    },
+    {
+        title: "다섯번째 영상",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 56,
+        id: 5,
+    },
+];
+
+export const trending = (req, res) => res.render("home", {pageTitle: "Home", videos: videos});
 export const see = (req, res) => {
-    return res.send(`watch Video #${req.params.id}`);
+    // console.log(res.send(`watch Video #${req.params.id}`));
+    const id = req.params.id;
+    const video = videos[id-1];
+    res.render("watch", {pageTitle: `Watching ${video.title}`, video: video});
 };
-export const edit = (req, res) => {
-    console.log(req.params);
-    res.send("watch edit");
-};
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("upload video");
 export const deleteVideo = (req, res) => {
--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const PORT = 4000;
 const app = express();
 
 
-app. set("view engine", "pug");
+app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src" + "/views");
 app.use("/", globalRouter);
 app.use("/users", userRouter)
